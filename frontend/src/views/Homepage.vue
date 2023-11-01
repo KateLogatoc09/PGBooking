e5f9d9996c04a34fd0819c15a214ef8bd145e93d
KateLogatoc09 <144669024+KateLogatoc09@users.noreply.github.com>
2023-11-01 18:57:31 +0800

    Additional
    addition

--- frontend/src/views/Homepage.vue
+++ frontend/src/views/Homepage.vue
@@ -4,7 +4,9 @@
 <heady/>
 <navy/>
 <bgsearch/>
+<about/>
 <service/>
+<hotels/>
 <footerr/>
 
   
@@ -15,11 +17,13 @@
 import heady from"@/components/heady.vue";
 import navy from"@/components/inc/navy.vue";
 import bgsearch from"@/components/inc/bgsearch.vue";
+import about from"@/components/content/about.vue";
 import service from"@/components/content/service.vue";
+import hotels from"@/components/content/hotels.vue";
 import footerr from"@/components/inc/footerr.vue";
 export default {
   components: {
-    heady,navy,bgsearch,service,footerr
+    heady,navy,bgsearch,service,about,hotels,footerr
   }
 };
 </script>
