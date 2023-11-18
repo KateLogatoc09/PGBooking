7d529525ecae072780655cc6b2b9b80c48c5b35f
Yuan Gabriel De Guzman <yuangabrieldeguzman@gmail.com>
2023-11-18 19:30:15 +0800

    Change the login design for hotel login

--- frontend/src/components/content/login.vue
+++ frontend/src/components/content/login.vue
@@ -1,59 +1,113 @@
 <template>
-    <div class="h-100 gradient-form" style="background-color: #eee;">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-xl-10">
-            <div class="card rounded-3 text-black">
-              <div class="row g-0">
-                <div class="col-lg-6">
-                  <div class="card-body p-md-5 mx-md-4">
-                    <div class="text-center">
-                      <!-- TODO: ADD THE LOGO HERE -->
-                      <!-- <img src="@/assets/assets/images/package-1.jpg" style="width: 185px;" alt="logo"> -->
-                      <h4 class="mt-1 mb-5 pb-1">PG Booking</h4>
+  <div class="back">
+    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+      <div class="container">
+        <div>
+          <div class="row g-5 align-items-center">
+            <div class="col-md-6 text-white" id="classy2">
+              <h6 class="text-white text-uppercase">Puerto Galera Tourism</h6>
+              <h1 class="text-white mb-4">Hotel Log In</h1>
+              <p class="mb-4">Welcome to Hotel Login</p>
+              <p class="mb-4">Fill up all the information needed to login to your account.</p>
+
+            </div>
+            <div class="col-md-6">
+              <h1 class="text-white mb-4 text-center" id="classy">Hotel Log In</h1>
+              <form @submit.prevent="log_tourist">
+                <div class="row g-3">
+                  <div v-if='!changed' class="col-md-12" id="color">
+                    <div class="form-floating">
+                      <input type="text" class="form-control bg-transparent" id="username" placeholder="Your Username"
+                        v-model="username" required>
+                      <label for="username">Your Username</label>
                     </div>
-
-                    <!-- TODO: ADD NYO NALANG YUNG LOGIN ROUTE DINE -->
-                    <!-- TODO: SA FORM ACTION PARA DUN SA PROCESSING NG LOGIN SA BACKEND -->
-                    <form action="" method="POST">
-                      <p>Please login to your account</p>
-                      <div class="form-outline mb-4">
-                        <input type="text" name="username" class="form-control" />
-                        <label class="form-label" for="username">Username</label>
-                      </div>
-
-                      <div class="form-outline mb-4">
-                        <input type="password" name="password" class="form-control" />
-                        <label class="form-label" for="password">Password</label>
-                      </div>
-
-                      <div class="row text-center pt-1 mb-5 pb-1">
-                        <button class="btn btn-primary btn-block fa-lg mb-3" type="submit">Log in</button><br>
-                        <a class="text-muted" href="#!">Forgot password?</a>
-                        <!-- TODO: DINE ILALAGAY KUNG MAY FORGOT PASSWORD TAYO SA BACKEND -->
-                      </div>
-
-                      <div class="row align-items-center justify-content-center pb-4">
-                        <p class="mb-0 me-2">Don't have an account?</p>
-                        <!-- TODO: DINE ILALAGAY YUNG SA SIGNUP ROUTE KUNG MERON SA HOTEL SIDE -->
-                        <button type="button" class="btn btn-block btn-outline-primary">Create new</button>
-                      </div>
-                    </form>
                   </div>
-                </div>
-                <div class="col-lg-6 d-flex align-items-center background-login" style="background-image: url('@/assets/assets/images/beach.jpg');background-repeat: no-repeat; background-size: cover;">
-                  <div class="text-dark px-3 py-4 p-md-5 mx-md-4">
-                    <h4 class="mb-4">Puerto Galera: Heart of Asia</h4>
-                    <p class="small mb-0">Nestled amidst verdant gardens and overlooking the azure Mindoro Strait, this renowned diving destination provides
-                    direct access to vibrant coral reefs, marine life, and shipwrecks. Luxurious villas blend nature with comfort, while the
-                    on-site restaurant serves a diverse culinary experience. Committed to sustainability, the resort offers eco-friendly
-                    adventures, making it an unforgettable destination for divers and nature enthusiasts alike.</p>
+                  <div v-else class="col-md-12" id="color">
+                    <div class="form-floating">
+                      <input type="email" class="form-control bg-transparent" id="email" placeholder="Your Email"
+                        v-model="email" required>
+                      <label for="email">Your Email</label>
+                    </div>
                   </div>
+                  <div class="col-md-12" id="color">
+                    <div class="form-floating">
+                      <input type="password" class="form-control bg-transparent" id="password" placeholder="Your Password"
+                        v-model="password" required>
+                      <label for="password">Your Password</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button class="btn btn-outline-light w-100 py-3" type="submit">Login Now</button>
+                  </div>
+                  <a @click='togglelog(), clearuname()' v-if="!changed" class="dark">Log in with your email instead?</a>
+                  <a @click='togglelog(), clearemail()' v-if="changed" class="dark">Log in with your username instead?</a>
+                  <router-link to="/Password_Recovery" tag="a" class="dark">Forgot Password?</router-link>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
+<script>
+import router from '@/router';
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      changed: false,
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    async log_tourist() {
+
+      try {
+        // TODO: Ilagay dine sa url yung api path para sa login 
+        const url = ""
+        const log_tour = await axios.post(url, {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        });
+
+        if (log_tour.data.msg === 'okay') {
+          sessionStorage.setItem("jwt", log_tour.data.token)
+          // TODO: PALITAN YUNG SA /tourist_account kung san yung URL na pupuntahan kapag Tama ang login
+          // router.push('/tourist_account');
+        } else if (log_tour.data.msg === 'wrong password.') {
+          alert(log_tour.data.msg);
+        } else {
+          alert(log_tour.data.msg);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+    },
+    togglelog() {
+      if (this.changed) {
+        this.changed = false;
+        return this.changed;
+      } else {
+        this.changed = true;
+        return this.changed;
+      }
+    },
+    clearuname() {
+      document.getElementById("username").value = "";
+    },
+    clearemail() {
+      document.getElementById("email").value = "";
+    }
+
+  }
+}
+</script>
